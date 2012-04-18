c5823d0dbe1ed02f0327854faa39837469615a99
Seb Armand <khepin@gmail.com>
2012-04-18 11:02:31 +0800

    switch from http to git as vendor downloading protocol (fixes DL errors on slow connections

--- bin/vendors.php
+++ bin/vendors.php
@@ -10,15 +10,15 @@ if (!is_dir(VENDOR_PATH)) {
 }
 
 $deps = array(
-    array('doctrine-orm', 'http://github.com/doctrine/doctrine2.git'),
-    array('doctrine-dbal', 'http://github.com/doctrine/dbal.git'),
-    array('doctrine-common', 'http://github.com/doctrine/common.git'),
-    array('doctrine-data-fixtures', 'http://github.com/doctrine/data-fixtures.git'),
+    array('doctrine-orm', 'git://github.com/doctrine/doctrine2.git'),
+    array('doctrine-dbal', 'git://github.com/doctrine/dbal.git'),
+    array('doctrine-common', 'git://github.com/doctrine/common.git'),
+    array('doctrine-data-fixtures', 'git://github.com/doctrine/data-fixtures.git'),
 
-    array('Symfony/Component/ClassLoader', 'http://github.com/symfony/ClassLoader.git', 'v2.0.9'),
-    array('Symfony/Component/Yaml', 'http://github.com/symfony/Yaml.git', 'v2.0.9'),
+    array('Symfony/Component/ClassLoader', 'git://github.com/symfony/ClassLoader.git', 'v2.0.9'),
+    array('Symfony/Component/Yaml', 'git://github.com/symfony/Yaml.git', 'v2.0.9'),
     
-    array('Mockery', 'https://github.com/padraic/mockery'),
+    array('Mockery', 'git://github.com/padraic/mockery'),
 );
 
 foreach ($deps as $dep) {
