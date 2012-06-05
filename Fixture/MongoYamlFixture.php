604f0bcc4c6fa1a9ddcda02173a730380570cca7
Sebastien Armand <khepin@gmail.com>
2012-06-05 20:03:20 +0800

    code refactoring

--- Fixture/MongoYamlFixture.php
+++ Fixture/MongoYamlFixture.php
@@ -2,57 +2,22 @@
 
 namespace Khepin\YamlFixturesBundle\Fixture;
 
-use Doctrine\Common\Persistence\ObjectManager;
 use Doctrine\Common\Util\Inflector;
 
 class MongoYamlFixture extends AbstractFixture {
 
-    public function load(ObjectManager $manager, $tags = null) {
-        if(!$this->hasTag($tags)){
-            return;
-        }
-        $class = $this->file['model'];
-        // Get the fields that are not "associations"
-        $metadata = $this->getMetaDataForClass($class);
-
-        foreach ($this->file['fixtures'] as $reference => $fixture_data) {
-            $object = $this->createObject($class, $fixture_data, $metadata);
-            $this->loader->setReference($reference, $object);
-            if(!$this->isReverseSaveOrder()){
-                $manager->persist($object);
-            }
-        }
-
-        if($this->isReverseSaveOrder()){
-            $refs = array_keys($this->file['fixtures']);
-            for($i = (count($refs) - 1); $i>=0; $i--){
-                $manager->persist($this->loader->getReference($refs[$i]));
-            }
-        }
-
-        $manager->flush();
-    }
-
-    public function getMetadataForClass($class){
-        $manager = $this->loader->getManager('mongodb');
-        $cmf = $manager->getMetadataFactory();
-
-        return $cmf->getMetaDataFor($class);
-    }
-    
     /**
-     * For fixtures that have relations to the same table, they need to appear
-     * in the opposite order that they need to be saved.
-     * @return boolean 
+     * Creates and returns one object based on the given data and metadata
+     *
+     * @param $class object's class name
+     * @param $data array of the object's fixture data
+     * @param $metadata the class metadata for doctrine
+     * @param $embedded true for embedded documents
+     * @return Object
      */
-    public function isReverseSaveOrder(){
-        if(!isset($this->file['save_in_reverse']) || $this->file['save_in_reverse'] == false){
-            return false;
-        }
-        return true;
-    }
-
-    public function createObject($class, $data, $metadata, $embedded = false){
+    public function createObject($class, $data, $metadata, $options =  array()){
+        // options to state if a document is to be embedded or persisted on its own
+        $embedded = isset($options['embed_one']) ? true : false;
         $mapping = array_keys($metadata->fieldMappings);
         // Instantiate new object
         $object = new $class;
@@ -79,7 +44,7 @@ class MongoYamlFixture extends AbstractFixture {
                             $embed_class = $metadata->fieldMappings[$field]['targetDocument'];
                             $embed_data = $value;
                             $embed_meta = $this->getMetaDataForClass($embed_class);
-                            $value = $this->createObject($embed_class, $embed_data, $embed_meta, true);
+                            $value = $this->createObject($embed_class, $embed_data, $embed_meta, array('embed_one' => true));
                         } else {
                             $value = $this->loader->getReference($value);
                         }
