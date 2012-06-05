013749bf420be3da83d480b1868592b75bb00e3b
Sebastien Armand <khepin@gmail.com>
2012-06-05 19:29:54 +0800

    add tests for embedded document

---
+++ tests/Khepin/Fixture/Document/Author.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Khepin\Fixture\Document;
+
+use Doctrine\ODM\MongoDB\Mapping\Annotations as ODM;
+
+/**
+ * @ODM\EmbeddedDocument
+ */
+class Author {
+    
+    /**
+     * @ODM\Id
+     */
+    private $id;
+    
+    /**
+     * @ODM\String
+     */
+    private $name;
+    
+    public function getId(){
+        return $this->id;
+    }
+    
+    public function getName(){
+        return $this->name;
+    }
+    
+    public function setName($name){
+        $this->name = $name;
+    }
+}
